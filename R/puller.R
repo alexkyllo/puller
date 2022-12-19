c7c45ff38081d013fa984053f034f9f1be2389ce
Alex Kyllo <alex.kyllo@gmail.com>
2022-12-19 09:06:33 -0800

    add AzureTokenCLI class and wrapper for get_kusto_token

--- R/puller.R
+++ R/puller.R
@@ -1,39 +1,46 @@
 #' Log into the Azure CLI by running the `az login` command.
+#' @param tenant The Azure Active Directory tenant name or GUID.
+#' @param use_device_code default FALSe. Set to TRUE if you wish to use device
+#' code flow instead of interactive web sign-in flow.
 #' @export
 az_login <- function(tenant, use_device_code = FALSE) {
     args <- c("login")
     if (!missing(tenant)) args <- c(args, paste("--tenant", tenant))
     if (use_device_code) args <- c(args, "--use-device-code")
-    execute_cmd(list(command = "az", args = args))
+    execute_az_cmd(list(command = "az", args = args))
 }
 
 #' Get an access token using the Azure CLI
-#'
 #' @param resource The URI of the Azure resource to which to authenticate.
-#'
+#' @param tenant The Azure Active Directory tenant name or GUID.
+#' @param use_cache default TRUE. If TRUE, tokens will be cached for reuse.
 #' @details Runs the `az account get-access-token` command to retrieve an access
 #' token from Azure via the Azure CLI. Presumes that you are signed in. If not,
 #' call `az_login()` first.
 #'
 #' @export
-get_cli_token <- function(resource) {
-    resource_name <- jsonlite::read_json(
-        path = paste0(resource, "/v1/rest/auth/metadata")
-    )
-    scope_command <- paste0(
-        "az account get-access-token --scope ",
-        resource_name$AzureAD$KustoServiceResourceId,
-        "/.default"
-    )
-    message(scope_command)
-    access_token <- jsonlite::parse_json(system(scope_command, intern = TRUE))
-    return(access_token)
+get_az_cli_token <- function(resource, tenant, use_cache = TRUE) {
+    if (missing(tenant))
+        AzureTokenCLI$new(resource = resource, use_cache = use_cache)
+    else
+        AzureTokenCLI$new(resource = resource, tenant = tenant,
+            use_cache = use_cache)
 }
-# TODO: fix
-# ERROR: User '' does not exist in MSAL token cache. Run `az login`.
-# Error: lexical error: invalid char in json text.
-#                                        NA
-#                      (right here) ------^
+
+#' Get an access token for Kusto using authorization code flow.
+#' @param cluster The cluster name or URI. e.g.: "mycluster.westus"
+#' @param tenant The Azure Active Directory tenant name or GUID.
+#' @param use_cache default TRUE. If TRUE, tokens will be cached for reuse.
+#' @export
+get_kusto_auth_token <- function(cluster, tenant = "common", use_cache = TRUE) {
+    if (startsWith(cluster, "https://")) {
+        resource <- cluster
+    } else {
+        resource <- glue::glue("https://{cluster}.kusto.windows.net")
+    }
+    AzureAuth::get_azure_token(resource = resource, tenant = tenant,
+        use_cache = use_cache, app = "db662dc1-0cfe-4e1c-a843-19a68e65be58")
+}
 
 #' Build the Azure CLI command to get an access token
 #' @param command default "az"
@@ -76,7 +83,7 @@ handle_az_cmd_errors <- function(cond) {
 #' Execute a command to the Azure CLI with arguments and handle error cases.
 #' @param cmd A list with the command in $command and a vector of arguments in
 #' $args.
-execute_cmd <- function(cmd) {
+execute_az_cmd <- function(cmd) {
     tryCatch(
         {
             message(cmd$command, " ", paste(cmd$args, collapse = " "), "\n")
@@ -92,6 +99,20 @@ execute_cmd <- function(cmd) {
             handle_az_cmd_errors(cond$message)
         }
     )
+}
+
+process_cli_response <- function(res, resource) {
+    # Parse the JSON from the CLI and fix the names to snake_case
+    ret <- jsonlite::parse_json(res)
+    tok_data <- list(
+        token_type = ret$tokenType,
+        access_token = ret$accessToken,
+        tenant = ret$tenant,
+        expires_on = as.numeric(as.POSIXct(ret$expiresOn))
+    )
+    # CLI doesn't return resource identifier so we need to pass it through
+    if (!missing(resource)) tok_data$resource <- resource
+    return(tok_data)
 }
 
 #' Run a Kusto query and export results to Azure Storage in Parquet or CSV
@@ -127,6 +148,12 @@ distributed=false
 }
 
 #' Execute the Kusto query and export the result to Azure Storage.
+#' @param tbl An instance of AzureKusto::tbl_kusto
+#' @param name_prefix The filename prefix to use for exported files.
+#' @param storage_uri The Azure Storage URI to export files to.
+#' @param key default "impersonate" which uses the account signed into Kusto to
+#' authenticate to Azure Storage. An Azure Storage account key.
+#' @param ... pass-through parameters to AzureKusto::run_query
 #' @export
 export.tbl_kusto <- function(tbl, name_prefix, storage_uri, key = "impersonate",
                              ...) {
@@ -156,8 +183,10 @@ wrap_sql_request <- function(query, sql_server, sql_database) {
     glue::glue("set notruncation; evaluate sql_request('{conn_str}', \"{query}\")")
 }
 
-query_kusto <- function(server_url, database, query) {
-    token <- get_cli_token(server_url)
+#' Query a Kusto server/database using an Azure CLI token by default.
+#' 
+query_kusto <- function(server_url, database, query, token) {
+    if (missing(token)) token <- get_az_cli_token(server_url)
     endpoint <- AzureKusto::kusto_database_endpoint(
         server = server_url,
         database = database,
@@ -170,3 +199,86 @@ query_kusto_sql <- function(sql_server, sql_database, server, database, query) {
     wrapped_query <- wrap_sql_request(query, sql_server, sql_database)
     query_kusto(server, database, wrapped_query)
 }
+
+#' A subclass of AzureAuth::AzureToken R6 class for tokens from Azure CLI.
+#' @docType class
+#' @export
+AzureTokenCLI <- R6::R6Class("AzureTokenCLI", inherit = AzureAuth::AzureToken,
+    # TODO: upstream this to the AzureAuth package.
+    public = list(
+        initialize = function(resource = NULL, tenant = NULL,
+            aad_host = "https://login.microsoftonline.com/", version = 1,
+            use_cache = NULL, token_args = list())
+        {
+            self$auth_type <- "cli"
+            self$version <- version
+            if (!is.null(resource)) {
+                if (self$version == 1) self$resource <- resource
+                else self$scope <- sapply(resource, AzureAuth::verify_v2_scope,
+                    USE.NAMES = FALSE)
+            }
+
+            self$aad_host <- aad_host
+            if (!is.null(tenant))
+                self$tenant <- AzureAuth::normalize_tenant(tenant)
+            self$token_args <- token_args
+            private$use_cache <- use_cache
+
+            # use_cache = NA means return dummy object:
+            # initialize fields, but don't contact AAD
+            if (is.na(use_cache)) return()
+
+            if (use_cache) private$load_cached_credentials()
+
+            # time of initial request for token: in case we need to set expiry
+            # time manually
+            request_time <- Sys.time()
+            if (is.null(self$credentials)) {
+                res <- private$initfunc(auth_info)
+                self$credentials <- private$process_response(res)
+            }
+            private$set_expiry_time(request_time)
+
+            if (private$use_cache)
+                self$cache()
+        }
+    ),
+    private = list(
+        initfunc = function(init_args) {
+            tryCatch(
+                {
+                    if (is.null(self$tenant)) {
+                        cmd <- build_az_token_cmd(resource = self$resource)
+                    } else {
+                        cmd <- build_az_token_cmd(resource = self$resource,
+                            tenant = self$tenant)
+                    }
+                    result <- execute_az_cmd(cmd)
+                    # result is a multi-line JSON string, concatenate together
+                    paste0(result)
+                },
+                warning = function(cond) {
+                    not_found <- grepl("not found", cond, fixed = TRUE)
+                    not_loggedin <- grepl("az login", cond, fixed = TRUE) |
+                        grepl("az account set", cond, fixed = TRUE)
+                    bad_resource <- grepl(
+                        "was not found in the tenant",
+                        cond,
+                        fixed = TRUE
+                    )
+                    if (not_found)
+                        message("Azure CLI not found on path.")
+                    else if (not_loggedin)
+                        message("Please run 'az login' to set up account.")
+                    else
+                        message("Failed to invoke the Azure CLI.")
+                }
+            )
+        },
+        process_response = function(res) {
+            processed <- process_cli_response(res, self$resource)
+            self$tenant <- processed$tenant
+            processed
+        }
+    )
+)
